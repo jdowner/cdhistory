e591b24cc5b4abe981503532188faefa8270c586
Joshua Downer <joshua.downer@gmail.com>
2014-10-20 07:53:02 -0400

    alias: simple tab completion

--- alias.bash
+++ alias.bash
@@ -3,8 +3,21 @@
 function cd(){
   if [[ ${1:0:1} = ":" ]]; then
     builtin cd $(cdhistory -r -n 1 -m ${1:1})
+    echo $(pwd)
   else
     cdhistory -a $1
     builtin cd $1
   fi
 }
+
+_cd_completion()
+{
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  if [[ "${COMP_CWORD}" -gt 2 ]]; then
+    COMPREPLY=()
+  elif [[ "${COMP_WORDS[1]}" == ":" ]]; then
+    COMPREPLY=( $(cdhistory -n 1 -m ${cur:1}) )
+  fi
+}
+
+complete -o dirnames -o nospace -F _cd_completion cd

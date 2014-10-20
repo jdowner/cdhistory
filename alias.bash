792353e17e9739dfcb08b3708af3845de4cf9fa5
Joshua Downer <joshua.downer@gmail.com>
2014-10-20 17:25:09 -0400

    alias: improved shadow function

--- alias.bash
+++ alias.bash
@@ -1,9 +1,16 @@
 # Over-ride the build cd function so that visited directories can be
 # recorded and used to quickly navigate to the most frequently visited.
 function cd(){
+  local dst
+
   if [[ ${1:0:1} = ":" ]]; then
-    builtin cd $(cdhistory -r -n 1 -m ${1:1})
-    echo $(pwd)
+    dst=$(cdhistory -n 1 -m ${1:1})
+    if [[ ! -d "${dst}" ]]; then
+      echo "Unable to find match"
+      return
+    fi
+    builtin cd ${dst}
+    echo ${dst}
   else
     cdhistory -a $1
     builtin cd $1

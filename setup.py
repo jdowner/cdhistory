a3811d7dca33f7cd832b3273b460442ca1b6f7b7
Joshua Downer <joshua.downer@gmail.com>
2015-07-16 07:03:15 -0400

    cdh: added setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
               'share/cdhistory.bash',
               ]),
         ],
+        scripts=['bin/cdhistory'],
         install_requires=[
             'docopt',
             'pep8',
